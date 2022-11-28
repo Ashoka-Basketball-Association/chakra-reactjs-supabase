ecf181156dd497772106f998c2f730760c67636e
Pranav132 <mais.06pranav@gmail.com>
2022-11-29 00:20:32 +0530

    teams page done

--- aba.github.io/src/components/TeamsGrid/ProductCard.js
+++ aba.github.io/src/components/TeamsGrid/ProductCard.js
@@ -12,14 +12,13 @@ import {
 
   
   export const ProductCard = (props) => {
-    const { product, rootProps } = props
-    const { name, imageUrl } = product
     const [visible, setVisible] = React.useState(false);
     const handler = () => setVisible(true);
     const closeHandler = () => {
-      setVisible(false);
-      console.log("closed");
+        setVisible(false);
+        console.log("closed");
     };
+
     return (
         <>
       <Stack
@@ -27,15 +26,14 @@ import {
           base: '4',
           md: '5',
         })}
-        {...rootProps}
         auto
         shadow onClick={handler}
       >
         <Box position="relative">
           <AspectRatio ratio={4 / 3}>
             <Image
-              src={imageUrl}
-              alt={name}
+              src={props.team.logo}
+              alt={props.team.name}
               draggable="false"
               fallback={<Skeleton />}
               borderRadius={useBreakpointValue({
@@ -56,22 +54,22 @@ import {
     >
       <Modal.Header>
         <Text id="modal-title" b size={24}>
-            A2Z
+            {props.team.name}
         </Text>
       </Modal.Header>
       <Modal.Body>
-        Lorem ipsum dolor sit amet, consectetur adip Lorem ips Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+        {props.team.desc}
         <br></br>
         <br></br>
         <Text b size={20}>
             Players
         </Text>
         <Table
-      aria-label="Example table with static content"
-      css={{
+        aria-label="Example table with static content"
+        css={{
         height: "auto",
         minWidth: "100%",
-      }}
+        }}
     >
       <Table.Header>
         <Table.Column>Name</Table.Column>
@@ -79,26 +77,14 @@ import {
         <Table.Column>Price</Table.Column>
       </Table.Header>
       <Table.Body>
-        <Table.Row key="1">
-          <Table.Cell>Siddh Veer Bakshi</Table.Cell>
-          <Table.Cell>1</Table.Cell>
-          <Table.Cell>100</Table.Cell>
-        </Table.Row>
-        <Table.Row key="2">
-          <Table.Cell>Krishna Bala</Table.Cell>
-          <Table.Cell>2</Table.Cell>
-          <Table.Cell>40</Table.Cell>
-        </Table.Row>
-        <Table.Row key="3">
-          <Table.Cell>Dhananjay Dogra</Table.Cell>
-          <Table.Cell>2</Table.Cell>
-          <Table.Cell>60</Table.Cell>
-        </Table.Row>
-        <Table.Row key="4">
-          <Table.Cell>Arjun Khanna</Table.Cell>
-          <Table.Cell>1</Table.Cell>
-          <Table.Cell>115</Table.Cell>
-        </Table.Row>
+        {props.team.players.map((player) => (
+            <Table.Row>
+                <Table.Cell><Text b>{player.name}</Text></Table.Cell>
+                <Table.Cell>{player.tier}</Table.Cell>
+                <Table.Cell textAlign="center">{player.price}</Table.Cell>
+            </Table.Row>
+        )
+        )}
       </Table.Body>
     </Table>
       </Modal.Body>
--- aba.github.io/src/components/TeamsGrid/TeamsGrid.js
+++ aba.github.io/src/components/TeamsGrid/TeamsGrid.js
@@ -1,28 +1,124 @@
 import { Box } from '@chakra-ui/react'
 import * as React from 'react'
 import { ProductCard } from './ProductCard'
-import { products } from './data'
 import { ProductGrid } from './ProductGrid'
+import { supabase } from '../../supabase/initialize'
+import { useState, useEffect } from 'react'
+import { Puff } from  'react-loader-spinner'
+import styled from 'styled-components';
 
-export const TeamsGrid = () => (
-  <Box
-    maxW="7xl"
-    mx="auto"
-    px={{
-      base: '4',
-      md: '8',
-      lg: '12',
-    }}
-    py={{
-      base: '6',
-      md: '8',
-      lg: '12',
-    }}
-  >
-    <ProductGrid>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </ProductGrid>
-  </Box>
-)
+const Wrapper = styled.div`
+  .wrapper{
+    height: 50vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+function containsObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i].id === obj.id) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export function TeamsGrid(){
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getData = async () => {
+        try{
+            let { data: test, error } = await supabase
+            .from('players')
+            .select(`
+            name,
+            desc,
+            tier,
+            position,
+            pic,
+            price,
+            team(
+                *
+            )
+            `)  
+            var teams = [];
+                
+                if (error){
+                    console.log(error);
+                }
+            for (var i in test){
+                if(!containsObject(test[i].team, teams)){
+                    var obj = test[i].team;
+                    obj.players = []
+                    teams.push(obj)
+                }
+            }
+
+            teams.sort((a,b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
+            
+            for (var j in test){
+                teams[test[j].team.id].players.push(test[j])
+            }
+
+            setData(teams);
+            setLoading(false);
+        }
+        catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+
+    if (loading) {
+    return (
+        <Wrapper>
+            <div className="wrapper">
+                <Puff
+                height="200"
+                width="200"
+                radisu={1}
+                color="#1b0c7a"
+                ariaLabel="puff-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                />
+            </div>
+        </Wrapper>
+    )
+}
+
+    return(
+        <Box
+        maxW="7xl"
+        mx="auto"
+        px={{
+        base: '4',
+        md: '8',
+        lg: '12',
+        }}
+        py={{
+        base: '6',
+        md: '8',
+        lg: '12',
+        }}
+    >
+        <ProductGrid>
+        {data.map((team) => (
+            <ProductCard key={team.id} team={team} />
+        ))}
+        </ProductGrid>
+    </Box>
+    )
+    }
