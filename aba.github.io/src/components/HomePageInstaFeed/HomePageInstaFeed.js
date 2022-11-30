ed9b3bfa63252eb42ebd045d4b1bc72663a7f883
sxddhvxxr <siddh.bakshi_ug23@ashoka.edu.in>
2022-11-30 20:45:37 +0530

    instafeed and culture unfinished

---
+++ aba.github.io/src/components/HomePageInstaFeed/HomePageInstaFeed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import InstagramFeed from 'react-ig-feed';
+import 'react-ig-feed/dist/index.css';
+const HomePageInstaFeed = () => {
+  return (
+    <>
+      <InstagramFeed
+        token="IGQVJVNEtwLXcwZAk50OW82QTBoMGs1M1FxYnpraXBnNEN5WWJGZAjJQX0lUY2cwNmtabUI3Q0tGQ3ExQ0ZAOYXlHX2Fyb1llQUhic1VOWE1LMG9ZATFBHVVZAGMHB2dGowemdzTWNiSl9fSEk1UHdIMlhoSwZDZD"
+        counter="12"
+      />
+    </>
+  );
+};
+
+export default HomePageInstaFeed;
--- aba.github.io/src/pages/HomePage/index.js
+++ aba.github.io/src/pages/HomePage/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import HomePageAbout from '../../components/HomePageAbout/HomePageAbout';
 import HomePageBanner from '../../components/HomePageBanner/HomePageBanner';
+import HomePageCulture from '../../components/HomePageCulture/HomePageCulture';
+import HomePageInstaFeed from '../../components/HomePageInstaFeed/HomePageInstaFeed';
 
 const HomePage = () => {
   return (
     <>
       <HomePageBanner />
       <HomePageAbout />
+      {/* <HomePageInstaFeed /> */}
+      {/* <HomePageCulture /> */}
     </>
   );
 };
